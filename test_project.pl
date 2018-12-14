c2d2b1368865dc70c0bcfedbbcb759bbe67cfbe5
alex <ilm88@mail.ru>
2018-12-14 12:19:35 +0500

    +hash

--- test_project.pl
+++ test_project.pl
@@ -10,23 +10,20 @@ use LWP::UserAgent;
 use HTTP::Request;
 use Data::Dumper qw(Dumper);
 
-print "Введите имя пользователя для получения статистики о его активности на GitHub.
+my $name;
+my @repos;
+my %response;
+print "Введите имя пользователя для получения данных об активности на GitHub.
 Или введите exit для выхода \n";
 while(my $line = <STDIN>) {   
+    
     chomp($line); # отсекаем символ \n
     last if($line eq "exit");
-    parse_response_commits ($line)
-   
-};
-sub uri_gen {
-    if ( $#_ == 0){
-        my $name=shift;
-        my $uri = "https://api.github.com/users/$name/repos";
-    }
-    elsif ( $#_ == 1) { 
-        my ($name, $repos)=@_;
-        my $uri = "https://api.github.com/repos/$name/$repos/commits";
-    };   
+    $name=$line;
+    say "Статистика пользователя. ", $name, ".";
+    get_repos_name ();
+    get_repos_commits ();
+    print_respons ();
 };
 
 sub api_github { 
@@ -38,44 +35,31 @@ sub api_github {
         return $response;
         }
     else{
-       print $response-> error_as_HTML;
+       print $response-> error_request_api_github;
        };  
 };
 sub get_repos_name {
-    my $uri = shift;
-    my $content;
-    my @repos;
+    my $uri = "https://api.github.com/users/$name/repos";
     my $response = api_github($uri);
-    $content=$response-> content;
+    my $content=$response-> content;
     @repos=$content=~m/"node\_id":"\w*=","name":"(.*?)"/img;
 };
 sub get_repos_commits {
-    my $uri = shift;
-    my $content;
-    my @commits;
-    my $response = api_github($uri);
-    $content=$response-> content;
-    @commits=$content=~m/"commit":(\S*?)"\},"committer"/img;
-    
-};
-sub parse_response_commits {
-    my $line=shift;  
-    my $uri = uri_gen ($line);
-    my @repos = get_repos_name ($uri); 
-    my @response;
     foreach my $repos (@repos){
-        my $uri = uri_gen ($line, $repos);
-        #say $uri; #test
-        @response = get_repos_commits ($uri);
-        #say Dumper \@response;
-        my $sumrep=$#response+1;
-        say "Репозиторий ", $repos, " коммитов ", $sumrep;
-    };   
+        my $uri = "https://api.github.com/repos/$name/$repos/commits";
+        my $response = api_github($uri);
+        my $content=$response-> content;
+        my @commits=$content=~m/"commit":\{"(\S*?)\},"committer"/img;  
+        foreach my $commit (@commits){
+            $commit=~m/"date":"(.*?)"/ig;
+            #if проверка на "свежесть"
+            $response{$name}{$repos}{$1}++;
+        };  
+    };
+        
 };
-sub repos_name { 
-    my $line=shift;  
-    my $uri = uri_gen ($line);
-    my @repos = get_repos_name ($uri);  
-    say Dumper \@repos; 
+sub table {
 };
-
+sub print_respons {
+    say Dumper \%response;
+};
